39d3ccfd90d7755f465f5fdde53634930cee8947
Alvin Zheng <azheng2019@126.com>
2022-06-16 13:01:53 +0800

    update Top Nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+  </header>
   <router-view />
 </template>
 
